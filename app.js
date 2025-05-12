958ce05706040732f6de4216c734bb2600a10688
fedebacci <federicobacci1999@gmail.com>
2025-05-12 14:04:30 +0200

    svolgimento punto 1

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+
+
+app.get('/', (req, res) => {
+    console.log(`Richiesta ricevuta a: / sulla porta: ${port}`);
+
+    response = "Server del mio blog";
+    res.send(response);
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Server del mio blog in ascolto sulla porta: ${port}`);
+})
